6d08310e6ce5a81680ca15921c8e4d1d7f1b6e83
Bradley Morin <bradleymorin@gmail.com>
2024-11-05 17:44:42 -0700

    Add Book constructor

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello world!");
+function Book(title, author, pageCount, haveRead) {
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.haveRead = haveRead;
+
+  this.info = function() {
+    var bookInfo = `"${this.title}" by ${this.author}, ${this.pageCount} pages, `
+
+    if (this.haveRead) {
+      bookInfo += "read"
+    }
+    else {
+      bookInfo += "not read yet"
+    }
+
+    return bookInfo;
+  }
+}
+
+const book = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
+const info = book.info();
+console.log(info);
