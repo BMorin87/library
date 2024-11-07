6116e5add78bcb70e691f02e13e8401c0ea1fa31
Bradley Morin <bradleymorin@gmail.com>
2024-11-06 17:29:22 -0700

    Add modal dialogue

--- script.js
+++ script.js
@@ -35,16 +35,50 @@ myLibrary.forEach(book => {
   const card = document.createElement("div");
   card.classList.add("book-card");
 
-  const cardContent = `
+  card.innerHTML = `
   <h2>${book.title}</h2>
   <p>Author: ${book.author}</p>
   <p>Page Count: ${book.pageCount}</p>
   <label><input type="checkbox"> I've read this</label>
   `;
-  card.innerHTML = cardContent;
   if (book.haveRead) {
     const checkBox = card.querySelector('input[type="checkbox"]')
     checkBox.checked = true;
   }
   bookContainer.appendChild(card);
 })
+
+const addBook = document.getElementById("add-book");
+addBook.addEventListener('click', showDialogue)
+
+function showDialogue() {
+  const dialogue = document.createElement('dialog');
+  dialogue.classList.add('modal-dialogue');
+
+  const dialogueContent = document.createElement('div');
+  dialogueContent.classList.add('modal-content');
+
+  const form = document.createElement('form');
+  form.innerHTML = `
+  <label for="title">Title:</label>
+  <input type="text" id="title" name="title"><br>
+  <label for="author">Author:</label>
+  <input type="text" id="author" name="author"><br>
+  <label for="pageCount">Page count:</label>
+  <input type="text" id="pageCount" name="pageCount"><br>
+  <label for="haveRead"><input type="checkbox" id="haveRead" name="haveRead">I've read this</label>
+  `;
+  dialogueContent.appendChild(form);
+
+  const closeButton = document.createElement('button');
+  closeButton.classList.add('close');
+  closeButton.textContent = 'X';
+  closeButton.addEventListener('click', () => {
+    dialogue.close();
+  });
+  dialogueContent.appendChild(closeButton);
+
+  dialogue.appendChild(dialogueContent);
+  document.body.appendChild(dialogue);
+  dialogue.showModal();
+}
