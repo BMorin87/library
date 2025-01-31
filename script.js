cc25c8047b02a5a1afbbb013466dc7f5aea79367
Bradley Morin <bradleymorin@gmail.com>
2025-01-31 13:00:24 -0700

    Refactor into Library and ScreenController classes.

--- script.js
+++ script.js
@@ -6,44 +6,65 @@ class Book {
     this.haveRead = haveRead;
   }
 
-  info = function() {
+  info = function () {
     var bookInfo = `"${this.title}" by ${this.author}, ${this.pageCount} pages, `;
 
     if (this.haveRead) {
       bookInfo += "read";
-    }
-    else {
+    } else {
       bookInfo += "not read yet";
     }
 
     return bookInfo;
+  };
+}
+
+class Library {
+  constructor(name = "My Library") {
+    this.SetName(name);
+    this.bookList = [];
+
+    this.InitializeLibrary(this.bookList);
+  }
+
+  SetName = (name) => {
+    this.name = name;
+  };
+
+  InitializeLibrary(bookList) {
+    this.AddBook("The Hobbit", "J.R.R. Tolkien", 295, true, bookList);
+    this.AddBook("Anathem", "Neal Stephenson", 642, true, bookList);
+    this.AddBook("Ada, or Ardor", "Vladimir Nabokov", 311, false, bookList);
+  }
+
+  AddBook(title, author, pageCount, haveRead) {
+    const newBook = new Book(title, author, pageCount, haveRead);
+    this.bookList.push(newBook);
   }
 }
 
-var myLibrary = [];
-initializeLibrary();
-updateLibraryDisplay(myLibrary);
+class ScreenController {
+  constructor() {
+    // Load a default library.
+    const library = new Library();
+    this.activeLibrary = library;
+    this.UpdateLibraryDisplay(library);
 
-const addBookButton = document.getElementById("add-book");
-addBookButton.addEventListener('click', showDialog);
+    // Add event listener to "Add book" button.
+    const addBookButton = document.getElementById("add-book");
+    addBookButton.addEventListener("click", () => this.ShowDialog());
+  }
 
-function initializeLibrary() {
-  addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true, myLibrary);
-  addBookToLibrary("Anathem", "Neal Stephenson", 642, true, myLibrary);
-  addBookToLibrary("Ada, or Ardor", "Vladimir Nabokov", 311, false, myLibrary);
-}
+  UpdateLibraryDisplay(library) {
+    const bookContainer = document.getElementById("book-container");
 
-function addBookToLibrary(title, author, pageCount, haveRead, library) {
-  const newBook = new Book(title, author, pageCount, haveRead);
-  library.push(newBook);
-}
+    library.bookList.forEach((book, index) => {
+      const card = this.CreateCard(book, index);
+      bookContainer.appendChild(card);
+    });
+  }
 
-
-function updateLibraryDisplay(library) {
-  const bookContainer = document.getElementById("book-container");
-
-  library.forEach((book, index) => {
-    // Create a card to display for each book in the library.
+  CreateCard(book, index) {
     const card = document.createElement("div");
     card.classList.add("book-card");
     card.dataset.index = index;
@@ -51,100 +72,135 @@ function updateLibraryDisplay(library) {
     <h2 class="title">${book.title}</h2>
     <p class="author">Author: ${book.author}</p>
     <p class="pageCount">Page Count: ${book.pageCount}</p>
-    <label><input type="checkbox" class="haveRead"> I've read this</label><br>
-    <button type="button" id="remove">Remove book</button>
+    <label><input type="checkbox" class="haveRead" name="haveRead"> I've read this</label><br>
+    <button type="button" class="remove">Remove book</button>
     `;
 
-    // Toggle the haveRead boolean if the user clicks the checkbox.
-    const checkBox = card.querySelector('input[type="checkbox"]')
-    if (book.haveRead) { checkBox.checked = true; }
-    checkBox.addEventListener('change', toggleHaveRead)
+    this.CreateEventListeners(card, book);
 
-    const removeButton = card.querySelector("#remove");
-    removeButton.addEventListener('click', removeBook);
+    return card;
+  }
 
-    bookContainer.appendChild(card);
-  })
-}
+  CreateEventListeners(card, book) {
+    const checkBox = card.querySelector('input[type="checkbox"]');
+    if (book.haveRead) {
+      checkBox.checked = true;
+    }
+    checkBox.addEventListener("change", () => this.ToggleHaveRead(card, book));
 
-function toggleHaveRead() {
-  // The checkbox is a grandchild of the display card.
-  const card = this.parentNode.parentNode;
-  const index = card.dataset.index;
-  myLibrary[index].haveRead = this.checked ? true : false;
-}
+    const removeButton = card.querySelector(".remove");
+    removeButton.addEventListener("click", () => this.RemoveBook(card));
+  }
 
-function removeBook() {
-  // Remove the book from the DOM.
-  const card = this.parentNode;
-  card.remove();
+  ToggleHaveRead(card, book) {
+    const checkbox = card.querySelector(".haveRead");
+    book.haveRead = checkbox.checked ? true : false;
+  }
 
-  // And from the internal library array.
-  const index = card.dataset.index;
-  myLibrary.splice(index, 1);
-}
+  RemoveBook(card) {
+    const index = card.dataset.index;
+    this.activeLibrary.bookList.splice(index, 1);
+    card.remove();
+  }
 
-function showDialog() {
-  // Create the modal dialog for the user to add books.
-  const dialog = document.createElement('dialog');
-  document.body.appendChild(dialog);
-  dialog.classList.add('modal-dialogue');
-  const dialogContent = document.createElement('div');
-  dialog.appendChild(dialogContent);
-  dialogContent.classList.add('modal-content');
-  const form = document.createElement('form');
-  dialogContent.appendChild(form);
-  form.innerHTML = `
-  <label for="title">Title:</label>
-  <input type="text" id="title" name="title"><br>
-  <label for="author">Author:</label>
-  <input type="text" id="author" name="author"><br>
-  <label for="pageCount">Page count:</label>
-  <input type="text" id="pageCount" name="pageCount"><br>
-  <label for="haveRead"><input type="checkbox" id="haveRead" name="haveRead">I've read this</label><br>
-  <button type="button" id="submit">Submit book</button>
-  `;
-  // Button for the user to submit a book to the library.
-  const submitButton = form.querySelector("#submit");
-  submitButton.addEventListener('click', () => {
-    submit();
+  ShowDialog() {
+    const dialogDiv = this.CreateModalDialog();
+    this.CreateDialogContent(dialogDiv);
+    this.AddTitleValidation(dialogDiv);
+    dialogDiv.showModal();
+  }
+
+  CreateModalDialog() {
+    const dialog = document.createElement("dialog");
+    dialog.classList.add("modal-dialogue");
+    document.body.appendChild(dialog);
+
+    return dialog;
+  }
+
+  CreateDialogContent(dialog) {
+    const dialogContent = document.createElement("div");
+    dialogContent.classList.add("modal-content");
+
+    const form = document.createElement("form");
+    form.innerHTML = `
+    <label for="title">Title:</label>
+    <input type="text" id="title" name="title" required minlength='2'>
+    <span id="titleError" class='error' aria-live="polite"></span><br>
+    <label for="author">Author:</label>
+    <input type="text" id="author" name="author"><br>
+    <label for="pageCount">Page count:</label>
+    <input type="text" id="pageCount" name="pageCount"><br>
+    <label for="haveRead"><input type="checkbox" id="haveRead" name="haveRead">I've read this</label><br>
+    <button type="button" id="submit">Submit book</button>
+    `;
+
+    // Button for the user to submit a book to the library.
+    const submitButton = form.querySelector("#submit");
+    submitButton.addEventListener("click", () => this.Submit(dialog));
+
+    // Button to back out of the modal dialog.
+    const closeButton = document.createElement("button");
+    closeButton.classList.add("close");
+    closeButton.textContent = "Close";
+    closeButton.addEventListener("click", () => {
+      dialog.close();
+      dialog.remove();
+    });
+
+    dialogContent.appendChild(form);
+    dialogContent.appendChild(closeButton);
+    dialog.appendChild(dialogContent);
+  }
+
+  AddTitleValidation(dialog) {
+    const form = dialog.querySelector("form");
+    const title = form.querySelector("#title");
+    const titleError = form.querySelector("#titleError");
+
+    function showError() {
+      if (title.validity.valueMissing) {
+        titleError.textContent = "Please enter the book's title.";
+      } else if (title.validity.tooShort) {
+        titleError.textContent = `Title should be at least ${title.minLength} characters.`;
+      }
+      titleError.className = "error active";
+    }
+
+    title.addEventListener("input", (event) => {
+      if (!title.validity.valid) {
+        showError();
+        event.preventDefault();
+      }
+    });
+  }
+
+  Submit(dialog) {
+    // Get the user's input book information.
+    const form = dialog.querySelector("form");
+    const title = form.querySelector("#title").value;
+    const author = form.querySelector("#author").value;
+    const pageCount = form.querySelector("#pageCount").value;
+    const haveReadInput = form.querySelector("#haveRead");
+    let haveRead = haveReadInput.checked ? true : false;
+
+    // Add the book to the internal library.
+    this.activeLibrary.AddBook(title, author, pageCount, haveRead);
+
+    // Display the library with the new book added.
+    this.ClearLibraryDisplay();
+    this.UpdateLibraryDisplay(this.activeLibrary);
+
     dialog.close();
     dialog.remove();
-  });
+  }
 
-  // Button to back out of the modal dialog.
-  const closeButton = document.createElement('button');
-  dialogContent.appendChild(closeButton);
-  closeButton.classList.add('close');
-  closeButton.textContent = 'Close';
-  closeButton.addEventListener('click', () => {
-    dialog.close();
-    dialog.remove();
-  });
-
-  dialog.showModal();
-}
-
-function submit() {
-  // Get the user's input book information.
-  const form = document.querySelector("form");
-  const title = form.querySelector('#title').value;
-  const author = form.querySelector('#author').value;
-  const pageCount = form.querySelector('#pageCount').value;
-  const haveReadInput = form.querySelector('#haveRead');
-  let haveRead = haveReadInput.checked ? true : false;
-
-  // Add the book to the internal library.
-  addBookToLibrary(title, author, pageCount, haveRead, myLibrary)
-
-  // Display the library with the new book added.
-  clearLibraryDisplay();
-  updateLibraryDisplay(myLibrary);
-}
-
-function clearLibraryDisplay() {
-  const bookContainer = document.getElementById("book-container");
-  while (bookContainer.firstChild) {
-    bookContainer.firstChild.remove();
+  ClearLibraryDisplay() {
+    const bookContainer = document.getElementById("book-container");
+    while (bookContainer.firstChild) {
+      bookContainer.firstChild.remove();
+    }
   }
 }
+
+new ScreenController();
